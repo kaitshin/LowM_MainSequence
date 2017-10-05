fixed masking and counting masked elements for mmt ha_nb921; got rid of unnecessary AP_rows masking

analysis/stack_data.py
@@ -15,9 +15,11 @@ OUTPUTS:
 
 import numpy as np
 from scipy.interpolate import interp1d
-def stack(ndarr_in, zspec_in, index, x0, xmin, xmax, ff='', AP_rows=[]):
+def stack(ndarr_in, zspec_in, index, x0, xmin, xmax, ff=''):
     ndarr = ndarr_in[index]
     zspec = zspec_in[index]
+
+    ndarr[np.where(ndarr.mask==True)] = np.nan
     
     x_rest   = np.arange(xmin, xmax, 0.1)
     new_grid = np.ndarray(shape=(len(ndarr), len(x_rest)))
@@ -28,36 +30,32 @@ def stack(ndarr_in, zspec_in, index, x0, xmin, xmax, ff='', AP_rows=[]):
     minz = min(x for x in zspec if x > 0)
     maxz = max(x for x in zspec if x < 9)
     print '### zspec:', minz, maxz
-    #deshifting to rest-frame wavelength
+    # deshifting to rest-frame wavelength
     for (row_num, ii) in zip(range(len(ndarr)), index):
-        #normalizing
+        # normalizing
         spec_test = ndarr[row_num]
 
-        #counts the non-masked elements of the array
-        if np.ma.MaskedArray.count(spec_test)==0:
+        # counts the completely masked elements of the array
+        if len(np.where(np.isnan(spec_test))[0]) == len(ndarr[row_num]):
             masked_spectra_len += 1
-        #endif
 
-        #interpolating a function for rest-frame wavelength and normalized y
+        # interpolating a function for rest-frame wavelength and normalized y
         x_test = x0/(1.0+zspec[row_num])
         f = interp1d(x_test, spec_test, bounds_error=False, fill_value=np.nan)
 
-        #finding the new rest-frame wavelength values from the interpolation
-        #and putting them into the 'new grid'
-        spec_interp = f(x_rest)
-        
-        # masking MMT NB921 Halpha stacking values if they're part of the 'bad' APs
-        if (ff and ff=='NB921' and ii in AP_rows):
-            mask_ii = np.where(x_rest > 6503)[0]
-            spec_interp[mask_ii] = np.nan
-            num_maskednb921 += 1
-            masked_index.append(ii)
-        #endif
+        # counts the completely masked elements of the nb921 spectrum
+        if ff == 'NB921':
+            nb921ii = np.where(x_test > 6500)[0]
+            if len(np.where(np.isnan(spec_test[nb921ii]))[0]) == len(nb921ii):
+                num_maskednb921 += 1
 
+        # finding the new rest-frame wavelength values from the interpolation
+        # and putting them into the 'new grid'
+        spec_interp = f(x_rest)
         new_grid[row_num] = spec_interp
     #endfor
 
-    #taking the average, column by column
+    # taking the average, column by column to 'stack'
     plot_grid_avg = np.nanmean(new_grid, axis=0)
 
     return x_rest, plot_grid_avg, [len(index)-masked_spectra_len, num_maskednb921], [index, masked_index], minz, maxz