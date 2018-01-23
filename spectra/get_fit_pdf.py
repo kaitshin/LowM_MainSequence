d2c923421edece1b1544bdb7255a108b2f47423d
Chun Ly <astro.chun@gmail.com>
2018-01-23 13:25:30 -0700

    spectra.get_fit_pdf: Read in FITS catalog files; Use match_nosort_str to crossmatch to get pages

--- spectra/get_fit_pdf.py
+++ spectra/get_fit_pdf.py
@@ -9,6 +9,7 @@ import sys, os
 import getpass
 
 from chun_codes import systime
+from chun_codes import match_nosort_str # + on 23/01/2018
 from chun_codes import exec_pdfmerge
 
 from os.path import exists
@@ -17,6 +18,7 @@ from astropy.io import fits
 
 import numpy as np
 
+import glob # + on 23/01/2018
 from astropy import log
 
 def main(MMT=False, Keck=False, silent=False, verbose=True):
@@ -45,24 +47,52 @@ def main(MMT=False, Keck=False, silent=False, verbose=True):
     -----
     Created by Chun Ly, 22 January 2018
      - Modified input MMT files
+    Created by Chun Ly, 23 January 2018
+     - Read in FITS catalog files
+     - Use match_nosort_str to crossmatch to get pages
     '''
     
     if silent == False: log.info('### Begin main : '+systime())
 
     if getpass.getuser() == 'cly':
-        g_dir0 = '/Users/cly/Google Drive/NASA_Summer2015/Spectra/'
+        g_dir0 = '/Users/cly/Google Drive/NASA_Summer2015/'
     else:
         g_dir0 = ''
 
+    s_dir0 = g_dir0 + 'Spectra/'
     if MMT:
-        pdf_files = glob.glob(g_dir0 + 'MMT/Plots/MMT*_line_fit.pdf')
-        cat_files = glob.glob(g_dir0 + 'MMT/Plots/MMT*_line_fit.fits')
+        pdf_files = glob.glob(s_dir0+'MMT/Plots/MMT*_line_fit.pdf')
+        cat_files = glob.glob(s_dir0+'MMT/Plots/MMT*_line_fit.fits')
+        spec_cov_file = g_dir0+'Composite_Spectra/MMT_spectral_coverage.txt'
 
     if Keck:
-        pdf_files = glob.glob(g_dir0 + 'Keck/Plots/DEIMOS_*_line_fit.pdf')
-        cat_files = glob.glob(g_dir0 + 'Keck/Plots/DEIMOS_*_line_fit.fits')
+        pdf_files = glob.glob(s_dir0+'Keck/Plots/DEIMOS_*_line_fit.pdf')
+        cat_files = glob.glob(s_dir0+'Keck/Plots/DEIMOS_*_line_fit.fits')
+        spec_cov_file = g_dir0+'Composite_Spectra/Keck_spectral_coverage.txt'
+
+    # Read in FITS catalogs | + on 23/01/2018
+    cat_data = []
+    for cc in range(len(cat_files)):
+        if silent == False: log.info('### Reading : '+cat_files[cc])
+        tmp = fits.getdata(cat_files[cc])
+        cat_data.append(tmp)
 
     n_files = len(pdf_files)
+
+    # Read in MMT/Keck spectral coverage file | + on 23/01/2018
+    if silent == False: log.info('### Reading : '+spec_cov_file)
+    spec_data = asc.read(spec_cov_file)
+
+    # Crossmatch to get samples | + on 23/01/2018
+    filt0 = ['NB704', 'NB711', 'NB816', 'NB921', 'NB973']
+    for filt in filt0:
+        idx     = np.where(spec_data['filter'] == filt)[0]
+        spec_ID = spec_data['AP'][idx]
+
+        pages = []
+        for cc in range(len(cat_files)):
+            m_idxa, m_idxb = match_nosort_str(spec_ID, cat_data[cc].AP)
+            pages.append(m_idxb+1)
 
     if silent == False: log.info('### End main : '+systime())
 #enddef
