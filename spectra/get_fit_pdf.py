cc34fdf3987b8a90002e9bcc2305f00432773c06
Chun Ly <astro.chun@gmail.com>
2018-01-23 17:03:21 -0700

    spectra.get_fit_pdf.main: Get pages and all exec_pdfmerge to generate PDF

--- spectra/get_fit_pdf.py
+++ spectra/get_fit_pdf.py
@@ -16,6 +16,8 @@ from os.path import exists
 from astropy.io import ascii as asc
 from astropy.io import fits
 
+from astropy.table import Table
+
 import numpy as np
 
 import glob # + on 23/01/2018
@@ -50,6 +52,7 @@ def main(MMT=False, Keck=False, silent=False, verbose=True):
     Created by Chun Ly, 23 January 2018
      - Read in FITS catalog files
      - Use match_nosort_str to crossmatch to get pages
+     - Get pages and all exec_pdfmerge to generate PDF
     '''
     
     if silent == False: log.info('### Begin main : '+systime())
@@ -63,12 +66,13 @@ def main(MMT=False, Keck=False, silent=False, verbose=True):
     if MMT:
         pdf_files = glob.glob(s_dir0+'MMT/Plots/MMT*_line_fit.pdf')
         cat_files = glob.glob(s_dir0+'MMT/Plots/MMT*_line_fit.fits')
-        spec_cov_file = g_dir0+'Composite_Spectra/MMT_spectral_coverage.txt'
 
     if Keck:
         pdf_files = glob.glob(s_dir0+'Keck/Plots/DEIMOS_*_line_fit.pdf')
         cat_files = glob.glob(s_dir0+'Keck/Plots/DEIMOS_*_line_fit.fits')
         spec_cov_file = g_dir0+'Composite_Spectra/Keck_spectral_coverage.txt'
+
+    print cat_files, pdf_files
 
     # Read in FITS catalogs | + on 23/01/2018
     cat_data = []
@@ -92,7 +96,14 @@ def main(MMT=False, Keck=False, silent=False, verbose=True):
         pages = []
         for cc in range(len(cat_files)):
             m_idxa, m_idxb = match_nosort_str(spec_ID, cat_data[cc].AP)
-            pages.append(m_idxb+1)
+            str_page = ",".join([str(test+1) for test in m_idxb])
+
+            t_tab = Table([spec_ID[m_idxa],cat_data[cc].AP[m_idxb]])
+            t_tab.pprint(max_lines=-1)
+            pages.append(str_page)
+
+        outfile = g_dir0+'Plots/Line_Fits/'+filt+'.pdf'
+        exec_pdfmerge(pdf_files, pages, outfile, merge=True)
 
     if silent == False: log.info('### End main : '+systime())
 #enddef
