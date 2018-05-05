b3e39ec3807b37b57f683001fe6013e52d37e40b
Kaitlyn Shin <kshin1105@gmail.com>
2018-05-04 21:26:38 -0700

    so i have a version to look back on. (1) reads in bins from stacked data files. (2) reads in files more intelligently. (3) fixes bins for low mass sources. (4) creates a bins table for no_spectra sources to facilitate determining which EBV corrections to apply. (5) a shit ton of pseudocode for applying EBV corrections for no_spectra sources, likely most of which will be tossed...

--- EBV_corrections.py
+++ EBV_corrections.py
@@ -1,56 +1,10 @@
-import numpy as np, matplotlib.pyplot as plt
-import plotting.general_plotting as general_plotting
 from astropy.io import fits as pyfits, ascii as asc
 from astropy.table import Table
 from create_ordered_AP_arrays import create_ordered_AP_arrays
+import numpy as np, matplotlib.pyplot as plt
+import plotting.general_plotting as general_plotting
 
-global FULL_PATH
-
-def get_stlrmass_lists(stlr_mass, inst_str0, inst_dict):
-    '''
-    Helper function for main. Returns the MMT+Keck 'stlrmass' bins
-    '''
-    masslist_MMT, masslist_Keck = np.array([]), np.array([])
-    for instr in ['MMT', 'Keck']:
-        index_list = general_plotting.get_index_list2(stlr_mass, inst_str0, inst_dict, instr)
-
-        masslist = []
-        for match_index in index_list:
-            subtitle = [min(stlr_mass[match_index]), max(stlr_mass[match_index])] # USE NUMBERS NOT STRINGS
-            masslist.append(subtitle)
-        #endfor
-
-        if instr == 'MMT':
-            masslist_MMT = np.array(masslist)
-        else:
-            masslist_Keck = np.array(masslist)
-    #endfor
-    return masslist_MMT, masslist_Keck
-
-
-def get_stlrmassZ_lists(stlr_mass, inst_str0, inst_dict):
-    '''
-    Helper function for main. Returns the MMT+Keck 'stlrmassZ' bins
-    '''
-    masslistZ_MMT, masslistZ_Keck = np.array([]), np.array([])
-    for instr in ['MMT', 'Keck']:
-        if instr=='MMT':
-            index_list = general_plotting.get_index_list3(stlr_mass, inst_str0, inst_dict, instr)
-        elif instr=='Keck':
-            index_list = general_plotting.get_index_list2(stlr_mass, inst_str0, inst_dict, instr)
-        
-        masslist = []
-        for match_index in index_list:
-            subtitle = [min(stlr_mass[match_index]), max(stlr_mass[match_index])]
-            masslist.append(subtitle)
-        #endfor
-
-        if instr == 'MMT':
-            masslistZ_MMT = np.array(masslist)
-        else:
-            masslistZ_Keck = np.array(masslist)
-    #endfor
-    return masslistZ_MMT, masslistZ_Keck
+FULL_PATH = '/Users/kaitlynshin/GoogleDrive/NASA_Summer2015/'
 
 
 def get_bins(masslist_MMT, masslist_Keck, massZlist_MMT, massZlist_Keck):
@@ -65,37 +19,31 @@ def get_bins(masslist_MMT, masslist_Keck, massZlist_MMT, massZlist_Keck):
     return massbins_MMT, massbins_Keck, massZbins_MMT, massZbins_Keck
 
 
-def fix_masses_out_of_range(masses_MMT, masses_Keck, massZs_MMT, massZs_Keck):
+def fix_masses_out_of_range(masses_MMT_ii, masses_Keck_ii, masstype):
     '''
     Fixes masses that were not put into bins because they were too low.
-    Reassigns them to the lowest mass bin.
+    Reassigns them to the lowest mass bin. 
+
+    Does this for both stlrmass bins and stlrmassZ bins.
     '''
-    masses_MMT = np.array([1 if x==0 else x for x in masses_MMT])
-    masses_Keck = np.array([1 if x==0 else x for x in masses_Keck])
-    massZs_MMT = np.array([1 if x==0 else x for x in massZs_MMT])
-    massZs_Keck = np.array([1 if x==0 else x for x in massZs_Keck])
+    if masstype=='stlrmass':
+        masses_MMT_ii = np.array([1 if x==0 else x for x in masses_MMT_ii])
+        masses_Keck_ii = np.array([1 if x==0 else x for x in masses_Keck_ii])
+    else: #=='stlrmassZ'
+        masses_MMT_ii = np.array(['1'+x[1:] if x[0]=='0' else x for x in masses_MMT_ii])
+        masses_Keck_ii = np.array(['1'+x[1:] if x[0]=='0' else x for x in masses_Keck_ii])
 
-    return masses_MMT, masses_Keck, massZs_MMT, massZs_Keck
-
-
-def add_Zs_to_bins(massZs_MMT, massZs_Keck, filts):
-    '''
-    Adds the Z componenet to the stlrrmassZ bin lists
-    '''
-    massZs_MMT = np.array([str(massZs_MMT[x])+'-'+filts[x] for x in range(len(filts))])
-    massZs_Keck = np.array([str(massZs_Keck[x])+'-'+filts[x] for x in range(len(filts))])
-
-    return massZs_MMT, massZs_Keck
+    return masses_MMT_ii, masses_Keck_ii
 
 
-def bins_table(indexes, NAME0, AP, stlr_mass, massbins_MMT, massbins_Keck, 
+def bins_table_no_spectra(indexes, NAME0, AP, stlr_mass, massbins_MMT, massbins_Keck, 
     massZbins_MMT, massZbins_Keck):
     '''
     Creates and returns a table of bins as such:
 
     NAME             stlrmass filter stlrmassbin_MMT stlrmassbin_Keck stlrmassZbin_MMT stlrmassZbin_Keck
     ---------------- -------- ------ --------------- ---------------- ---------------- -----------------
-    Ha-NB704_005332  6.78     NB704  1               1                1-NB704           1-NB704
+    Ha-NB973_178201  8.4      NB973  4               3                2-NB973          2-NB973
 
     TODO: for 'indexes == yes_spectra', include S/N arrays etc.?
     '''
@@ -104,34 +52,131 @@ def bins_table(indexes, NAME0, AP, stlr_mass, massbins_MMT, massbins_Keck,
 
     # get redshift bins
     filts = np.array([x[x.find('Ha-')+3:x.find('Ha-')+8] for x in names])
+    
+    # this ensures that NB704+NB921 dual emitters will be placed in Ha-NB704 bins
+    #  since these sources are likely OIII-NB704 and Ha-NB921
+    dual_iis = [x for x in range(len(names)) if 'Ha-NB704' in names[x] and 'Ha-NB921' in names[x]]
+    filts[dual_iis] = 'NB704'
+    
+    # this ensures that the NB816+NB921 dual emitter will be placed in the NB921 bin
+    #  we decided this based on visual inspection of the photometry
+    dual_ii2 = [x for x in range(len(names)) if 'Ha-NB816' in names[x] and 'Ha-NB921' in names[x]]
+    filts[dual_ii2] = 'NB921'
 
-    # get stlrmass and stlrmassZ bins
-    masses_MMT = np.digitize(masses, massbins_MMT)
-    masses_Keck = np.digitize(masses, massbins_Keck)
-    massZs_MMT = np.digitize(masses, massZbins_MMT)
-    massZs_Keck = np.digitize(masses, massZbins_Keck)
-    masses_MMT, masses_Keck, massZs_MMT, massZs_Keck = fix_masses_out_of_range(masses_MMT, 
-        masses_Keck, massZs_MMT, massZs_Keck)
-    massZs_MMT, massZs_Keck = add_Zs_to_bins(massZs_MMT, massZs_Keck, filts)
+    # get stlrmass bins
+    masses_MMT_ii = np.digitize(masses, massbins_MMT)
+    masses_Keck_ii = np.digitize(masses, massbins_Keck)
+    masses_MMT, masses_Keck = fix_masses_out_of_range(masses_MMT_ii, masses_Keck_ii, 'stlrmass')
+    
+    # get stlrmassZ bins
+    ii0 = 0
+    ii1 = 0
+    massZs_MMT = np.array([])
+    massZs_Keck = np.array([])
+    for ff in ['NB704','NB711','NB816','NB921','NB973']:
+        jj = len(np.where(ff==massZlist_filts_MMT)[0])        
+        
+        good_filt_iis = np.array([x for x in range(len(filts)) if filts[x]==ff])
+        
+        if ff=='NB973':
+            jj += 1
+        
+        mass_MMT_iis  = np.digitize(masses[good_filt_iis], massZbins_MMT[ii0:ii0+jj])
+        massZ_MMT_iis = np.array([str(x)+'-'+ff for x in mass_MMT_iis])
+        massZs_MMT  = np.append(massZs_MMT, massZ_MMT_iis)
+        
+        ii0+=jj
+        if 'NB9' in ff:
+            kk = len(np.where(ff==massZlist_filts_Keck)[0])
 
+            if ff=='NB973':
+                kk += 1
+                
+            mass_Keck_iis  = np.digitize(masses[good_filt_iis], massZbins_Keck[ii1:ii1+kk])
+            massZ_Keck_iis = np.array([str(x)+'-'+ff for x in mass_Keck_iis])
+            massZs_Keck = np.append(massZs_Keck, massZ_Keck_iis)
+            
+            ii1+=kk
+        else:
+            massZs_Keck = np.append(massZs_Keck, np.array(['N/A']*len(good_filt_iis)))
+            masses_Keck[good_filt_iis] = -99
+    
+    # putting sources with m < m_min_bin in the lowest-massZ mass bin
+    massZs_MMT, massZs_Keck = fix_masses_out_of_range(massZs_MMT, massZs_Keck, 'stlrmassZ')
+
+        
     tab0 = Table([names, masses, filts, masses_MMT, masses_Keck, massZs_MMT, massZs_Keck], 
         names=['NAME', 'stlrmass', 'filter', 'stlrmassbin_MMT', 'stlrmassbin_Keck', 
-        'stlrmassZbin_MMT', 'stlrmassZbin_Keck'])
+               'stlrmassZbin_MMT', 'stlrmassZbin_Keck'])
 
     return tab0
 
 
-def EBV_corrs_no_spectra():
+def EBV_corrs_no_spectra(tab_no_spectra):
     '''
     '''
-    # if there's no stlrmass, use filts for EBV corrections
-    # if filts == 704, 711, or 816, use stlrmassZ MMT Hg/Hb lines for EBV corrs
-    # if filts == 921, 973 use stlrmassZ MMT Hg/Hb or Keck Ha/Hb for EBV corrs ... ?
+    EBV_corrs = np.array([-100.0]*len(tab_no_spectra))
+
+    # if there's no mass:
+    no_mass_iis = np.array([x for x in range(len(tab_no_spectra)) if tab_no_spectra['stlrmass'][x] < 0])
+    if len(no_mass_iis) > 0:
+        print 'TODO: implement using redshift bins to apply EBV corrections'
+        break
+
+    # if there is mass:
+    #  if filt == 'NB704', 'NB711', or 'NB816':
+    for filt in ['NB704', 'NB711', 'NB816']:
+        filt_ii = np.array([x for x in range(len(tab_no_spectra)) if tab_no_spectra['filter'][x]==filt])
+
+        # use stlrmassZ MMT ha/hb if possible
+        temp_ii = np.array([x for x in range(len(filt_ii)) if '0-' not in tab_no_spectra['stlrmassZbin_MMT'][filt_ii][x]])
+
+        # tab_filt_ii = [x for x in range(len(mmt_mz_EBV_hahb[:5])) if mmt_mz['filter'][x]==filt][0]
+        # EBV_corrs[filt_ii][temp_ii] = mmt_mz_EBV_hahb[tab_filt_ii]
+
+        # else use stlrmass MMT ha/hb
+        temp_ii = np.array([x for x in range(len(filt_ii)) if '0-' in tab_no_spectra['stlrmassZbin_MMT'][filt_ii][x]])
+        for ii in temp_ii:
+            new_ii = filt_ii[ii]
+            tab_filt_ii = tab_no_spectra['stlrmassbin_MMT'][new_ii] - 1
+            EBV_corrs[new_ii] = mmt_m_EBV_hahb[tab_filt_ii]
+    
+    #  elif filt == 'NB921':
+    filt_ii = np.array([x for x in range(len(tab_no_spectra)) if tab_no_spectra['filter'][x]=='NB921'])
+    #  use stlrmassZ Keck ha/hb if possible
+    tab_filt = [x for x in range(len(keck_mz_EBV_hahb)) if keck_mz['filter'][x]=='NB921']
+    temp_ii = np.array([x for x in range(len(filt_ii)) if '0-' not in tab_no_spectra['stlrmassZbin_Keck'][filt_ii][x]])
+    sub_tab = tab_no_spectra['stlrmassZbin_Keck'][filt_ii][temp_ii]
+    for ii in range(len(tab_filt)):
+        matched = np.array([x for x in range(len(sub_tab)) if sub_tab[x] == str(ii+1)+'-'+filt])
+        EBV_corrs[filt_ii[matched]] = keck_mz_EBV_hahb[ii]
+    if len(temp_ii) != len(filt_ii):
+        print 'TODO: implement the following condititions'
+    #         else use stlrmassZ MMT hg/hb
+    #         else use stlrmass Keck ha/hb
+    #         else use stlrmass MMT hg/hb
+        break
+
+    #  elif filt == 'NB973':
+    filt_ii = np.array([x for x in range(len(tab_no_spectra)) if tab_no_spectra['filter'][x]=='NB973'])
+    #  use stlrmassZ Keck ha/hb if possible
+    tab_filt = [x for x in range(len(keck_mz_EBV_hahb)) if keck_mz['filter'][x]=='NB973']
+    temp_ii = np.array([x for x in range(len(filt_ii)) if '0-' not in tab_no_spectra['stlrmassZbin_Keck'][filt_ii][x]])
+    sub_tab = tab_no_spectra['stlrmassZbin_Keck'][filt_ii][temp_ii]
+    for ii in range(len(tab_filt)):
+        matched = np.array([x for x in range(len(sub_tab)) if sub_tab[x] == str(ii+1)+'-'+filt])
+        EBV_corrs[filt_ii[matched]] = keck_mz_EBV_hahb[ii]
+    if len(temp_ii) != len(filt_ii):
+        print 'TODO: implement the following condititions'
+    #         else use stlrmassZ MMT hg/hb
+    #         else use stlrmass Keck ha/hb
+    #         else use stlrmass MMT hg/hb
+        break
+
+    return EBV_corrs
 
 
 def main():
-    FULL_PATH = '/Users/kaitlynshin/GoogleDrive/NASA_Summer2015/'
-
     # reading in data
     nbia = pyfits.open(FULL_PATH+'Catalogs/NB_IA_emitters.nodup.colorrev.fix.fits')
     nbiadata = nbia[1].data
@@ -146,6 +191,12 @@ def main():
     data_dict = create_ordered_AP_arrays(AP_only = True)
     AP = data_dict['AP']
 
+    # defining other useful data structs
+    inst_dict = {}
+    inst_dict['MMT']  = ['MMT,FOCAS,','MMT,','merged,','MMT,Keck,']
+    inst_dict['Keck'] = ['merged,','Keck,','Keck,Keck,','Keck,FOCAS,','Keck,FOCAS,FOCAS,','Keck,Keck,FOCAS,']
+
+
     # limit all data to Halpha emitters only
     ha_ii = np.array([x for x in range(len(NAME0)) if 'Ha' in NAME0[x]])
     NAME0     = NAME0[ha_ii]
@@ -154,27 +205,63 @@ def main():
     stlr_mass = stlr_mass[ha_ii]
     AP        = AP[ha_ii]
 
-    # defining other useful data structs
-    inst_dict = {}
-    inst_dict['MMT'] = ['MMT,FOCAS,','MMT,','merged,','MMT,Keck,']
-    inst_dict['Keck'] = ['merged,','Keck,','Keck,Keck,','Keck,FOCAS,','Keck,FOCAS,FOCAS,','Keck,Keck,FOCAS,']
-    masslist_MMT, masslist_Keck = get_stlrmass_lists(stlr_mass, inst_str0, inst_dict)
-    massZlist_MMT, massZlist_Keck = get_stlrmassZ_lists(stlr_mass, inst_str0, inst_dict)
-    massbins_MMT, massbins_Keck, massZbins_MMT, massZbins_Keck = get_bins(masslist_MMT, 
-        masslist_Keck, massZlist_MMT, massZlist_Keck)
-    
+
     # getting indexes for sources with and without spectra
     no_spectra  = np.where((zspec0 <= 0) | (zspec0 > 9))[0]
     yes_spectra = np.where((zspec0 >= 0) & (zspec0 < 9))[0]
 
+    # getting rid of special case:
+    bad_highz_gal = np.where(NAME0[no_spectra]=='Ha-NB816_174829_Ha-NB921_187439_Lya-IA598_163379')[0]
+    no_spectra = np.delete(no_spectra, bad_highz_gal)
+
+
+    # reading in EBV data tables & getting relevant EBV cols
+    mmt_z   = asc.read(FULL_PATH+'Composite_Spectra/Redshift/MMT_stacked_spectra_data.txt')
+    mmt_z_EBV_hahb = np.array(mmt_z['E(B-V)_hahb'])
+    mmt_z_EBV_hghb = np.array(mmt_z['E(B-V)_hghb'])
+    
+    keck_z  = asc.read(FULL_PATH+'Composite_Spectra/Redshift/Keck_stacked_spectra_data.txt')
+    keck_z_EBV_hahb = np.array(keck_z['E(B-V)_hahb'])
+    
+    mmt_m   = asc.read(FULL_PATH+'Composite_Spectra/StellarMass/MMT_all_five_data.txt')
+    mmt_m_EBV_hahb = np.array(mmt_m['E(B-V)_hahb'])
+    mmt_m_EBV_hghb = np.array(mmt_m['E(B-V)_hghb'])
+    
+    keck_m  = asc.read(FULL_PATH+'Composite_Spectra/StellarMass/Keck_all_five_data.txt')
+    keck_m_EBV_hahb = np.array(keck_m['E(B-V)_hahb'])
+    
+    mmt_mz  = asc.read(FULL_PATH+'Composite_Spectra/StellarMassZ/MMT_stlrmassZ_data.txt')
+    mmt_mz_EBV_hahb = np.array(mmt_mz['E(B-V)_hahb'])
+    mmt_mz_EBV_hghb = np.array(mmt_mz['E(B-V)_hghb'])
+    
+    keck_mz = asc.read(FULL_PATH+'Composite_Spectra/StellarMassZ/Keck_stlrmassZ_data.txt')
+    keck_mz_EBV_hahb = np.array(keck_mz['E(B-V)_hahb'])
+
+
+    # mass 'bin' lists made by reading in from files generated by the stack plots
+    masslist_MMT  = np.array([x.split('-') for x in mmt_m['stlrmass_bin']], dtype=float) 
+    masslist_Keck = np.array([x.split('-') for x in keck_m['stlrmass_bin']], dtype=float)
+
+    # same with massZ 'bin' lists (also getting the filts)
+    massZlist_MMT  = np.array([x.split('-') for x in mmt_mz['stlrmass_bin'] if x!='N/A'], dtype=float)
+    massZlist_Keck = np.array([x.split('-') for x in keck_mz['stlrmass_bin']], dtype=float)
+    massZlist_filts_MMT  = np.array([mmt_mz['filter'][x] for x in range(len(mmt_mz)) if mmt_mz['stlrmass_bin'][x]!='N/A'])
+    massZlist_filts_Keck = np.array([keck_mz['filter'][x] for x in range(len(keck_mz))])
+
+    # splitting the 'bin' lists from above into a flattened bins array
+    massbins_MMT, massbins_Keck, massZbins_MMT, massZbins_Keck = get_bins(masslist_MMT, 
+        masslist_Keck, massZlist_MMT, massZlist_Keck)
+    
+
     # getting tables of which bins the sources fall into (for eventual EBV corrections)
-    tab_no_spectra = bins_table(no_spectra, NAME0, AP, stlr_mass, 
+    tab_no_spectra = bins_table_no_spectra(no_spectra, NAME0, AP, stlr_mass, 
         massbins_MMT, massbins_Keck, massZbins_MMT, massZbins_Keck)
-    tab_yes_spectra = bins_table(yes_spectra, NAME0, AP, stlr_mass, 
-        massbins_MMT, massbins_Keck, massZbins_MMT, massZbins_Keck)
+    
+    # tab_yes_spectra = bins_table(yes_spectra, NAME0, AP, stlr_mass, 
+    #     massbins_MMT, massbins_Keck, massZbins_MMT, massZbins_Keck)
 
     # start getting those corrections!
-    # EBV_corrs_no_spectra(no_spectra, NAME0, AP, stlr_mass)
+    # EBV_corrs_no_spectra(no_spectra)
     # def EBV_corrs_for_no_spectra_sources(Pass in the identified sources w/o spectra): # pass in the instrument(s) as well?
     #     apply EBV correction based on bin (consider Keck vs. MMT, which lines, etc)
     #     return dust-corrected fluxes for the sources
@@ -189,6 +276,8 @@ def main():
     #         apply EBV correction
 
     #     return dust-corrected fluxes for the sources
+
+    # MMT_z_data = asc.read()
 
     
 if __name__ == '__main__':
