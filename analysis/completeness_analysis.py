c4574008c286a289621ae0e7b3260f0d3459d591
Chun Ly <astro.chun@gmail.com>
2019-05-01 16:08:37 -0700

    analysis.completeness_analysis.mag_vs_mass: Get continuum magnitudes and plot against stellar mass

--- analysis/completeness_analysis.py
+++ analysis/completeness_analysis.py
@@ -50,8 +50,9 @@ def mag_vs_mass(silent=False, verbose=True):
     # NB Ha emitter sample for ID
     NB_file = path0 + 'Main_Sequence/mainseq_corrections_tbl (1).txt'
     log.info("Reading : "+NB_file)
-    NB_tab   = asc.read(NB_file)
-    NB_Ha_ID = NB_tab['ID'].data - 1 # Relative to 0 --> indexing
+    NB_tab     = asc.read(NB_file)
+    NB_HA_Name = NB_tab['NAME0'].data
+    NB_Ha_ID   = NB_tab['ID'].data - 1 # Relative to 0 --> indexing
 
     # Read in stellar mass results table
     FAST_file = path0 + \
@@ -67,6 +68,32 @@ def mag_vs_mass(silent=False, verbose=True):
     log.info("Reading : "+NB_catfile)
     NB_catdata = fits.getdata(NB_catfile)
     NB_catdata = NB_catdata[NB_Ha_ID]
+
+    cont_mag = np.zeros(len(NB_catdata))
+
+    fig, ax = plt.subplots(ncols=2, nrows=2)
+
+    filts = ['NB704','NB711','NB816','NB921','NB973']
+
+    for filt in filts:
+        log.info('### Working on : '+filt)
+        NB_idx = [ii for ii in range(len(NB_tab)) if 'Ha-'+filt in NB_HA_Name[ii]]
+        print(" Size : ", len(NB_idx))
+        #print(NB_catdata[filt+'_CONT_MAG'])[NB_idx]
+        cont_mag[NB_idx] = NB_catdata[filt+'_CONT_MAG'][NB_idx]
+
+
+    NB704711_idx = [ii for ii in range(len(NB_tab)) if 'Ha-NB7' in NB_HA_Name[ii]]
+    ax[0][0].scatter(cont_mag[NB704711_idx], logM_NB_Ha[NB704711_idx])
+
+    NB816_idx = [ii for ii in range(len(NB_tab)) if 'Ha-NB816' in NB_HA_Name[ii]]
+    ax[0][1].scatter(cont_mag[NB816_idx], logM_NB_Ha[NB816_idx])
+
+    NB921_idx = [ii for ii in range(len(NB_tab)) if 'Ha-NB921' in NB_HA_Name[ii]]
+    ax[1][0].scatter(cont_mag[NB921_idx], logM_NB_Ha[NB921_idx])
+
+    NB973_idx = [ii for ii in range(len(NB_tab)) if 'Ha-NB973' in NB_HA_Name[ii]]
+    ax[1][1].scatter(cont_mag[NB973_idx], logM_NB_Ha[NB973_idx])
     
     if silent == False: log.info('### End mag_vs_mass : '+systime())
 #enddef
