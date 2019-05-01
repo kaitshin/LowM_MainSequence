b6af553aee7dd79ce19b7397f53a5363a14442b5
Chun Ly <astro.chun@gmail.com>
2019-05-01 15:32:25 -0700

    analysis.completeness_analysis: Add mag_vs_mass function

---
+++ analysis/completeness_analysis.py
@@ -0,0 +1,68 @@
+"""
+completeness_analysis
+====
+
+A set of Python 2.7 codes for completeness analysis of NB-selected galaxies
+in the M*-SFR plots
+"""
+
+import sys, os
+
+from chun_codes import systime
+
+from os.path import exists
+
+from astropy.io import fits
+from astropy.io import ascii as asc
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from astropy import log
+
+def mag_vs_mass(silent=False, verbose=True):
+
+    '''
+    Compares optical photometry against stellar masses to get relationship
+
+    Parameters
+    ----------
+
+    silent : boolean
+      Turns off stdout messages. Default: False
+
+    verbose : boolean
+      Turns on additional stdout messages. Default: True
+
+    Returns
+    -------
+
+    Notes
+    -----
+    Created by Chun Ly, 1 May 2019
+    '''
+
+    if silent == False: log.info('### Begin mag_vs_mass : '+systime())
+
+    path0 = '/Users/cly/Google Drive/NASA_Summer2015/'
+
+    # NB Ha emitter sample for ID
+    NB_file = path0 + 'Main_Sequence/mainseq_corrections_tbl (1).txt'
+    log.info("Reading : "+NB_file)
+    NB_tab   = asc.read(NB_file)
+    NB_Ha_ID = NB_tab['ID'].data - 1 # Relative to 0 --> indexing
+
+    # Read in stellar mass results table
+    FAST_file = path0 + \
+                'FAST/outputs/NB_IA_emitters_allphot.emagcorr.ACpsf_fast.GALEX.fout'
+    log.info("Reading : "+FAST_file)
+    FAST_tab = asc.read(FAST_file)
+
+    logM = FAST_tab['col7'].data
+
+    logM_NB_Ha = logM[NB_Ha_ID]
+    
+    if silent == False: log.info('### End mag_vs_mass : '+systime())
+#enddef
+
