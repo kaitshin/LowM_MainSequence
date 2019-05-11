227dd4e0ce2da84d654158ab3338ec2105632124
Kaitlyn Shin <kshin1105@gmail.com>
2019-05-11 13:53:19 -0400

    added oiii4363 lines

--- plotting/hg_hb_ha_plotting.py
+++ plotting/hg_hb_ha_plotting.py
@@ -69,7 +69,7 @@ def subplots_setup(ax, ax_list, label, subtitle, num, pos_flux=0, flux=0,
     elif ftitle=='NB921' and num%3==1 and subtitle[10] != '8':
         ymaxval = max(ax.get_ylim())
         ax_list[num-1].plot([4341,4341],[-1,ymaxval],'k',alpha=0.7,zorder=1)
-        # ax_list[num-1].plot([4363,4363],[-1,ymaxval],'k:',alpha=0.4,zorder=1)
+        ax_list[num-1].plot([4363,4363],[-1,ymaxval],'k:',alpha=0.4,zorder=1)
         ax_list[num].plot([4861,4861],[-1,ymaxval],'k',alpha=0.7,zorder=1)
         if subtitle[10]=='6':
             [a.set_ylim([-0.05, ymaxval]) for a in ax_list[num-1:num+1]]
@@ -80,7 +80,7 @@ def subplots_setup(ax, ax_list, label, subtitle, num, pos_flux=0, flux=0,
         if bintype!='StellarMassZ' or ftitle!='NB973':
             [a.set_ylim(ymax=ymaxval) for a in ax_list[num-2:num]]
         ax_list[num-2].plot([4341,4341],[0,ymaxval],'k',alpha=0.7,zorder=1)
-        # ax_list[num-2].plot([4363,4363],[0,ymaxval],'k:',alpha=0.4,zorder=1)
+        ax_list[num-2].plot([4363,4363],[0,ymaxval],'k:',alpha=0.4,zorder=1)
         ax_list[num-1].plot([4861,4861],[0,ymaxval],'k',alpha=0.7,zorder=1)
         ax_list[num].plot([6563,6563], [0,ymaxval],'k',alpha=0.7,zorder=1)
         ax_list[num].plot([6548,6548],[0,ymaxval], 'k:',alpha=0.4,zorder=1)
@@ -102,13 +102,13 @@ def subplots_setup(ax, ax_list, label, subtitle, num, pos_flux=0, flux=0,
         ymaxval = max(ax.get_ylim())
         [a.set_ylim(ymax=ymaxval) for a in ax_list[num-1:num]]
         ax_list[num-1].plot([4341,4341],[0,ymaxval],'k',alpha=0.7,zorder=1)
-        # ax_list[num-1].plot([4363,4363],[0,ymaxval],'k:',alpha=0.4,zorder=1)
+        ax_list[num-1].plot([4363,4363],[0,ymaxval],'k:',alpha=0.4,zorder=1)
         ax_list[num].plot([4861,4861],[0,ymaxval],'k',alpha=0.7,zorder=1)
     elif bintype=='StellarMassZ' and ftitle=='NB973' and num%3==1:
         ymaxval = max(ax.get_ylim())
         [a.set_ylim(ymax=ymaxval) for a in ax_list[num-1:num]]
         ax_list[num-1].plot([4341,4341],[0,ymaxval],'k',alpha=0.7,zorder=1)
-        # ax_list[num-1].plot([4363,4363],[0,ymaxval],'k:',alpha=0.4,zorder=1)
+        ax_list[num-1].plot([4363,4363],[0,ymaxval],'k:',alpha=0.4,zorder=1)
         ax_list[num].plot([4861,4861],[0,ymaxval],'k',alpha=0.7,zorder=1)
     if num%3!=0:
         ax.set_yticklabels([])
