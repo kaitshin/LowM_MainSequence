cd25ee57f448a14a993a13aba94dd687dc9c23b0
Kaitlyn Shin <kshin1105@gmail.com>
2019-08-07 06:49:43 -0400

    got rid of unused code

--- plot_mainseq_UV_Ha_comparison.py
+++ plot_mainseq_UV_Ha_comparison.py
@@ -67,208 +67,6 @@ def get_flux(ID, lambda_arr):
     return newflux
 
 
-# def get_lnu(filt_index_haii, ff):
-#     '''
-#     Calls get_flux with an array of redshifted wavelengths in order to get
-#     the corresponding flux values. Those f_lambda values are then converted
-#     into f_nu values, which is in turn converted into L_nu, the log of which
-#     is returned as nu_lnu.
-#     '''
-#     centr_filts = {'NB7':((7045.0/HA - 1) + (7126.0/HA - 1))/2.0, 
-#         'NB816':8152.0/HA - 1, 'NB921':9193.0/HA - 1, 'NB973':9749.0/HA - 1}
-
-#     ID = corrID[filt_index_haii]
-#     zspec = corrzspec0[filt_index_haii]
-
-#     goodz = np.where((zspec >= 0) & (zspec < 9))[0]
-#     badz  = np.where((zspec <= 0) | (zspec > 9))[0]
-
-#     tempz = np.zeros(len(filt_index_haii))
-#     tempz[goodz] = zspec[goodz]
-#     tempz[badz] = centr_filts[ff]
-
-#     lambda_arr = (1+tempz)*1500
-
-#     f_lambda = get_flux(ID, lambda_arr)
-#     f_nu = f_lambda*(1E-19*(lambda_arr**2*1E-10)/(constants.c.value))
-#     log_L_nu = np.log10(f_nu*4*np.pi) + \
-#         2*np.log10(cosmo.luminosity_distance(tempz).to(u.cm).value)
-
-#     return log_L_nu
-
-
-# def get_nu_lnu(filt_index_haii, ff):
-#     '''
-#     Calls get_lnu to get log(L_nu) and multiplied by log(nu), which is
-#     then returned as log(nu_lnu)
-#     '''
-#     log_L_nu = get_lnu(filt_index_haii, ff)
-#     return (np.log10(constants.c.value) - np.log10((1500*u.AA).to(u.m).value)) \
-#         + log_L_nu
-
-
-# def make_scatter_plot(nu_lnu, l_ha, ff, ltype):
-#     '''
-#     Makes a scatter plot (by filter) of nu_lnu vs l_ha, then saves and
-#     closes the figure.
-
-#     There is a value in the filter NB921 which has flux=='NAN'. That is
-#     ignored.
-#     '''
-#     plt.scatter(nu_lnu, l_ha, color='b', edgecolor='k', s=12)
-#     plt.gca().minorticks_on()
-#     plt.gca().tick_params(axis='both', which='both', direction='in')
-#     plt.xlabel('log['+r'$\nu$'+'L'+r'$_{\nu}$'+'(1500 '+r'$\AA$'+')]')
-#     plt.ylabel('log[L'+r'$_{H\alpha}$'+']')    
-#     plt.xlim(36.0, 48.0)
-#     plt.ylim(37.0, 44.0)
-#     plt.savefig(FULL_PATH+'Plots/main_sequence_UV_Ha/'+ff+'_'+ltype+
-#         fileend+'.pdf')
-#     plt.close()
-
-
-# def make_ratio_plot(nu_lnu, l_ha, stlr, ff, ltype):
-#     '''
-#     Makes a ratio plot (by filter) of stellar mass vs. (nu_lnu/l_ha), then
-#     saves and closes the figure.
-
-#     There is a value in the filter NB921 which has flux=='NAN'. That is
-#     ignored.
-#     '''
-#     ratio = nu_lnu-l_ha
-#     plt.scatter(stlr, ratio, s=12)
-#     plt.gca().minorticks_on()
-#     plt.gca().tick_params(axis='both', which='both', direction='in')
-#     plt.xlabel('log[M/M'+r'$_{\odot}$'+']')
-#     plt.ylabel('log['+r'$\nu$'+'L'+r'$_{\nu}$'+'/L(H'+r'$\alpha$'+')'+']')
-#     plt.savefig(FULL_PATH+'Plots/main_sequence_UV_Ha/ratios/'+ff+'_'+ltype+
-#         fileend+'.pdf')
-#     plt.close()
-
-
-# def make_all_ratio_legend(filtlabel):
-#     '''
-#     Makes a legend for the plot with all ratios (as a function of mass) of
-#     all the filters on the same plot.
-
-#     red='NB704', orange='NB711', green='NB816', blue='NB921', purple='NB973'
-#     '''
-#     import matplotlib.patches as mpatches
-
-#     red_patch = mpatches.Patch(color='red', label='H'+r'$\alpha$'+'-NB7 '
-#                                +filtlabel['NB7'], alpha=0.5)
-#     orange_patch = mpatches.Patch(color='orange', label='H'+r'$\alpha$'+'-NB816 '
-#                                  +filtlabel['NB816'], alpha=0.5)
-#     green_patch = mpatches.Patch(color='green', label='H'+r'$\alpha$'+'-NB921 '
-#                                 +filtlabel['NB921'], alpha=0.5)
-#     blue_patch = mpatches.Patch(color='blue', label='H'+r'$\alpha$'
-#                                   +'-NB973 '+filtlabel['NB973'], alpha=0.5)
-
-#     legend0 = plt.legend(handles=[red_patch,orange_patch,green_patch,
-#                                   blue_patch],fontsize=9,
-#                          loc='lower right', frameon=False)
-#     plt.gca().add_artist(legend0)
-
-
-# def get_binned_stats(xposdata, yposdata):
-#     '''
-#     From the min to the max x-values of the graph, 'bins' of interval 0.25
-#     are created. If there are more than 3 x values in a particular bin, the
-#     average of the corresponding y values are plotted, and their std dev
-#     values are plotted as error bars as well.
-
-#     In order to clearly stand out from the rainbow scatter plot points, these
-#     'binned' points are black.
-#     '''
-#     bins = np.arange(min(plt.xlim()), max(plt.xlim())+0.25, 0.25)
-#     for bb in range(len(bins)-1):
-#         valid_index = np.array([x for x in range(len(xposdata)) if
-#                                 xposdata[x] >= bins[bb] and xposdata[x] <
-#                                 bins[bb+1]])
-#         if len(valid_index) > 3:
-#             xpos = np.mean((bins[bb], bins[bb+1]))
-#             ypos = np.mean(yposdata[valid_index])
-#             yerr = np.std(yposdata[valid_index])
-#             plt.scatter(xpos, ypos, facecolor='k', edgecolor='none', alpha=0.7)
-#             plt.errorbar(xpos, ypos, yerr=yerr, ecolor='k', alpha=0.7,
-#                          fmt='none')
-
-
-# def make_all_ratio_plot(L_ha, ltype, xarr_type='stlr'):
-    '''
-    Similar as make_ratio_plot, except each filter is plotted on the graph,
-    and sources with good zspec are filled points while those w/o good zspec
-    are plotted as empty points. get_binned_stats and make_all_ratio_legend
-    are called, before the plot is duly modified, saved, and closed.
-    '''
-    # print ltype, '('+xarr_type+')'
-    # xposdata = np.array([])
-    # yposdata = np.array([])
-    # filtlabel = {}
-    # for (ff, cc) in zip(['NB7','NB816','NB921','NB973'], color_arr):
-    #     print ff
-
-    #     if ff == 'NB816': # GALEX file ID#4411 has flux==0
-    #         filt_index_haii = np.array([x for x in range(len(corr_tbl)) if ff in
-    #             corrfilts[x] and corrNAME0[x] != 'Ha-NB816_172306_OII-NB921_176686'])
-    #     else:
-    #         filt_index_haii = np.array([x for x in range(len(corr_tbl)) if ff in
-    #             corrfilts[x]])
-    #     l_ha = L_ha[filt_index_haii]
-
-    #     zspec = corrzspec0[filt_index_haii]
-    #     if xarr_type=='stlr':
-    #         xpos_arr = corrstlr0[filt_index_haii]
-    #     elif xarr_type=='sfr':
-    #         xpos_arr = corr_sfr[filt_index_haii]
-    #     else:
-    #         raise ValueError('Incorrect xarr_type provided (must be either \'stlr\' or \'sfr\'')
-
-    #     nu_lnu = get_nu_lnu(filt_index_haii, ff)
-    #     lmbda = (1500*u.AA).to(u.um).value
-    #     K_1500 = (2.659*(-2.156 + 1.509/lmbda - 0.198/lmbda**2 + 0.011/lmbda**3)+ 4.05)
-    #     A_1500 = K_1500 * corr_tbl['EBV'].data[filt_index_haii]
-    #     nu_lnu += 0.4*A_1500 # (dust correction: A_V = A(1500AA) = 10.33)
- 
-    #     ratio = nu_lnu-l_ha
-    #     xposdata = np.append(xposdata, xpos_arr)
-    #     yposdata = np.append(yposdata, ratio)
-
-    #     good_z = np.array([x for x in range(len(zspec)) if zspec[x] > 0. and
-    #                        zspec[x] < 9.])
-    #     bad_z  = np.array([x for x in range(len(zspec)) if zspec[x] <= 0. or
-    #                        zspec[x] >= 9.])
-    #     filtlabel[ff] = '('+str(len(good_z))+', '+str(len(bad_z))+')'
-
-    #     plt.scatter(xpos_arr[good_z], ratio[good_z], facecolor=cc, edgecolor='none',
-    #                 alpha=0.5, s=12)
-    #     plt.scatter(xpos_arr[bad_z], ratio[bad_z], facecolor='none', edgecolor=cc,
-    #                 linewidth=0.5, alpha=0.5, s=12)
-
-
-    # get_binned_stats(xposdata, yposdata)
-    # plt.gca().minorticks_on()
-    # plt.gca().tick_params(axis='both', which='both', direction='in')
-    # plt.ylabel('log['+r'$\nu$'+'L'+r'$_{\nu}$'+'(1500 '+r'$\AA$'+')/L'
-    #            +r'$_{H\alpha}$'+']')
-    # make_all_ratio_legend(filtlabel)
-    # plt.plot(plt.xlim(), [2.05, 2.05], 'k--', alpha=0.3, linewidth=3.0)
-    # if xarr_type=='stlr':
-    #     plt.xlim(4, 11)
-    #     plt.ylim(-2.5, 4)
-    #     plt.xlabel('log[M/M'+r'$_{\odot}$'+']')
-    #     plt.savefig(FULL_PATH+'Plots/main_sequence_UV_Ha/ratios/all_filt_'+ltype+
-    #                 fileend+'.pdf')
-    # elif xarr_type=='sfr':
-    #     plt.xlabel('log(SFR[H'+r'$\alpha$'+']/M'+r'$_{\odot}$'+' yr'+r'$^{-1}$'+')')
-    #     plt.savefig(FULL_PATH+'Plots/main_sequence_UV_Ha/ratios/all_filt_'+ltype+
-    #                 '_with_SFRs'+fileend+'.pdf')
-    # plt.close()
-
-
-
-#################
-
 def get_LUV(corrID, corrzspec0, centr_filts, filt_index_haii, ff):
     '''
     '''
@@ -374,7 +172,47 @@ def get_UV_SFR(corr_tbl):
     return log_SFR_LUV
 
 
-#############
+def plot_SFR_ratios(log_SFR_HA, log_SFR_UV, corr_tbl):
+    '''
+    '''
+    jlee_logSFR_ratio = np.array([0.2,0.17,0.07,-0.02,-.1,-.23,-.46,-.49,-1.29])
+    jlee_logSFRHa = np.array([0.25,-0.25,-0.75,-1.25,-1.75,-2.25,-2.75,-3.5,-4.5])
+    log_SFR_ratio = log_SFR_HA - log_SFR_UV
+    stlr_mass = corr_tbl['stlr_mass'].data
+
+    f, axarr = plt.subplots(2,1)
+    ax0 = axarr[0]
+    ax1 = axarr[1]
+
+    # plotting data
+    ax0 = plot_ff_zz_color_filled(ax0, log_SFR_HA, log_SFR_ratio, corr_tbl)
+    ax1 = plot_ff_zz_color_filled(ax1, stlr_mass, log_SFR_ratio, corr_tbl)
+
+    xlims0 = [min(log_SFR_HA)-0.2, max(log_SFR_HA)+0.2]
+    xlims1 = [min(stlr_mass)-0.2, max(stlr_mass)+0.2]
+    ylims = [min(log_SFR_ratio)-0.4, max(log_SFR_ratio)+0.4]
+    [ax.axhline(0, color='k') for ax in [ax0,ax1]]
+
+    # plotting relation from Lee+08
+    xtmparr0 = np.linspace(xlims0[0], xlims0[1], 10)
+    ax0.plot(jlee_logSFRHa, jlee_logSFR_ratio, 'ks')
+    ax0.plot(xtmparr0, 0.26*xtmparr0+0.3, 'k--')
+
+
+    # final touches
+    ax0.set_xlabel('log SFR(Ha)')
+    ax0.set_ylabel('log[SFR(Ha)/SFR(UV)]')
+
+    ax1.set_xlabel('stlr_mass')
+    ax1.set_ylabel('log[SFR(Ha)/SFR(UV)]')
+
+    [ax.tick_params(axis='both', labelsize='10', which='both',
+        direction='in') for ax in [ax0,ax1]]
+    f.set_size_inches(6,10)
+    ax0.legend(frameon=False, loc='best')
+    plt.tight_layout()
+    plt.savefig(FULL_PATH+'Plots/main_sequence_UV_Ha/SFR_ratio.pdf')
+
 
 def main():
     '''
@@ -410,10 +248,6 @@ def main():
     centr_filts = {'NB7':((7045.0/HA - 1) + (7126.0/HA - 1))/2.0, 
         'NB816':8152.0/HA - 1, 'NB921':9193.0/HA - 1, 'NB973':9749.0/HA - 1}
 
-    # jlee_logSFR_ratio = np.array([0.2,0.17,0.07,-0.02,-.1,-.23,-.46,-.49,-1.29])
-    # jlee_logSFRHa = np.array([0.25,-0.25,-0.75,-1.25,-1.75,-2.25,-2.75,-3.5,-4.5])
-    # jlee_logSFRUV = np.array([-2.21,-1.45,-0.7,-0.4,-0.26,-1.94,-2.04,-2.05,-2.52,-2.3])
-
 
     # getting SFR values
     log_SFR_LUV = get_UV_SFR(corr_tbl)
@@ -434,6 +268,8 @@ def main():
 
     # plotting
     plot_SFR_comparison(log_SFR_HA, log_SFR_UV, corr_tbl)
+    plot_SFR_ratios(log_SFR_HA, log_SFR_UV, corr_tbl)
+
 
 if __name__ == '__main__':
     main()
