74f6379743638beedfe543fa1c39b30fdf3fdf39
Chun Ly <astro.chun@gmail.com>
2020-07-21 17:03:41 -0700

    monte_carlo.main: Changes to output content #11
    - Additional documentation
    - Improvement with dict use

--- analysis/completeness/monte_carlo.py
+++ analysis/completeness/monte_carlo.py
@@ -10,8 +10,11 @@ from .properties import dict_prop_maker, derived_properties
 
 npz_MCnames = ['EW_seed', 'logEW_MC_ref', 'x_MC0_ref', 'BB_MC0_ref',
                'BB_sig_ref', 'sig_limit_ref', 'NB_sel_ref', 'NB_nosel_ref',
-               'EW_flag_ref', 'flux_ref', 'logM_ref', 'NIIHa_ref',
+               'EW_flag_ref']
+'''
+               'flux_ref', 'logM_ref', 'NIIHa_ref',
                'logOH_ref', 'HaFlux_ref', 'HaLum_ref', 'logSFR_ref']
+'''
 
 
 def mock_ones(arr0, Nmock):
@@ -34,8 +37,8 @@ def random_mags(t_seed, rand_shape, mag_ref, sig_ref):
            mock_ones(sig_ref, N_rep)
 
 
-def main(int_dict, npz_MC_file, ss_range, mass_dict, norm_dict, filt_dict,
-         EW_dict, NB_MC, lum_dist, mylog, redo=False):
+def main(int_dict, npz_MC_file, mock_sz, ss_range, mass_dict, norm_dict,
+         filt_dict, EW_dict, NB_MC, lum_dist, mylog, redo=False):
 
     ff = int_dict['ff']
     mm = int_dict['mm']
@@ -52,7 +55,7 @@ def main(int_dict, npz_MC_file, ss_range, mass_dict, norm_dict, filt_dict,
         np.random.seed(EW_seed)
         rand0 = np.random.normal(0.0, 1.0, size=norm_dict['Ngal'])
 
-        # This is not H-alpha
+        # Randomize based on log-normal EW distribution for Ngal (ref). Not H-alpha EW.
         logEW_MC_ref = logEW_mean[mm] + logEW_sig[ss] * rand0
         stats_log(logEW_MC_ref, "logEW_MC_ref", mylog)
 
@@ -66,17 +69,19 @@ def main(int_dict, npz_MC_file, ss_range, mass_dict, norm_dict, filt_dict,
         NB_sel_ref, NB_nosel_ref, \
             sig_limit_ref = NB_select(ff, norm_dict['NB_ref'], x_MC0_ref)
 
+        # Flag array to indicate true galaxies meet selection requirements
         EW_flag_ref = np.zeros(norm_dict['Ngal'])
         EW_flag_ref[NB_sel_ref] = 1
 
+        # Broad-band magnitudes for input sample
         BB_MC0_ref = norm_dict['NB_ref'] + x_MC0_ref
         BB_sig_ref = get_sigma(BB_MC0_ref, cont_lim[ff], sigma=3.0)
 
-        dict_prop = dict_prop_maker(norm_dict['NB_ref'], BB_MC0_ref,
-                                    x_MC0_ref, filt_dict, filt_corr[ff],
-                                    mass_int, lum_dist)
-        _, flux_ref, logM_ref, NIIHa_ref, logOH_ref, HaFlux_ref, \
-            HaLum_ref, logSFR_ref = derived_properties(**dict_prop)
+        # Combine photometry to get dictionary for derived properties input
+        dict_prop_ref = dict_prop_maker(norm_dict['NB_ref'], BB_MC0_ref,
+                                        x_MC0_ref, filt_dict, filt_corr[ff],
+                                        mass_int, lum_dist)
+        der_prop_dict_ref = derived_properties(suffix='_ref', **dict_prop_ref)
 
         if exists(npz_MC_file):
             mylog.info("Overwriting : " + npz_MC_file)
@@ -86,42 +91,57 @@ def main(int_dict, npz_MC_file, ss_range, mass_dict, norm_dict, filt_dict,
         npz_MCdict = {}
         for name in npz_MCnames:
             npz_MCdict[name] = eval(name)
+        npz_MCdict.update(der_prop_dict_ref)  # Add the derived properties dictionary
         np.savez(npz_MC_file, **npz_MCdict)
+
+        der_prop_dict_ref = derived_properties(**dict_prop_ref)
     else:
         if not redo:
             mylog.info("File found : " + npz_MC_file)
             npz_MC = np.load(npz_MC_file)
 
-            for key0 in npz_MC.keys():
-                cmd1 = key0 + " = npz_MC['" + key0 + "']"
-                exec (cmd1)
+            npz_MCdict = dict(npz_MC)
 
-            dict_prop = dict_prop_maker(norm_dict['NB_ref'], BB_MC0_ref,
-                                        x_MC0_ref, filt_dict, filt_corr[ff],
-                                        mass_int, lum_dist)
+            dict_prop_ref = dict_prop_maker(norm_dict['NB_ref'],
+                                            npz_MCdict['BB_MC0_ref'],
+                                            npz_MCdict['x_MC0_ref'],
+                                            filt_dict, filt_corr[ff],
+                                            mass_int, lum_dist)
 
     BB_seed = ff + 5
     mylog.info("seed for broadband, mm=%i ss=%i : %i" % (mm, ss, BB_seed))
-    BB_MC = random_mags(BB_seed, mock_sz, BB_MC0_ref, BB_sig_ref)
+
+    # Broad-band mocked magnitudes
+    BB_MC = random_mags(BB_seed, mock_sz, npz_MCdict['BB_MC0_ref'],
+                        npz_MCdict['BB_sig_ref'])
     stats_log(BB_MC, "BB_MC", mylog)
 
-    x_MC = BB_MC - NB_MC
+    x_MC = BB_MC - NB_MC  # NB color excess (mocked)
     stats_log(x_MC, "x_MC", mylog)
 
+    # Selection based on mocked magnitudes
     NB_sel, NB_nosel, sig_limit = NB_select(ff, NB_MC, x_MC)
 
+    # Flag array to indicate if mock galaxies meet selection requirements
     EW_flag0 = np.zeros(mock_sz)
     EW_flag0[NB_sel[0], NB_sel[1]] = 1
 
     # Not sure if we should use true logEW or the mocked values
+    # Currently using mocked values
     # logEW_MC = mock_ones(logEW_MC_ref, Nmock)
 
-    dict_prop['NB'] = NB_MC
-    dict_prop['BB'] = BB_MC
-    dict_prop['x'] = x_MC
-    logEW_MC, flux_MC, logM_MC, NIIHa, logOH, HaFlux_MC, HaLum_MC,\
-        logSFR_MC = derived_properties(std_mass_int=std_mass_int,
-                                       **dict_prop)
-    stats_log(logEW_MC, "logEW_MC", mylog)
-    stats_log(flux_MC, "flux_MC", mylog)
-    stats_log(HaFlux_MC, "HaFlux_MC", mylog)
+    # Replace NB, BB and x with mocked values
+    dict_prop_MC = dict_prop_ref.copy()
+
+    dict_prop_MC['NB'] = NB_MC
+    dict_prop_MC['BB'] = BB_MC
+    dict_prop_MC['x'] = x_MC
+
+    der_prop_dict_MC = derived_properties(std_mass_int=std_mass_int,
+                                          **dict_prop_MC)
+    stats_log(der_prop_dict_MC['logEW'], "logEW_MC", mylog)
+    stats_log(der_prop_dict_MC['NB_flux'], "flux_MC", mylog)
+    stats_log(der_prop_dict_MC['Ha_Flux'], "HaFlux_MC", mylog)
+
+    return dict_prop_ref, der_prop_dict_ref, npz_MCdict, dict_prop_MC,\
+           der_prop_dict_MC, EW_flag0, NB_sel, NB_nosel
